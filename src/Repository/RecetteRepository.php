9d20d8efeea11c5b59e5e0a4f141db41cccdf879
Pepinunco <jeanpac31@gmail.com>
2024-05-22 14:29:11 +0200

    ajout de commentaires, et page d'error, et error404

--- src/Repository/RecetteRepository.php
+++ src/Repository/RecetteRepository.php
@@ -23,64 +23,93 @@ class RecetteRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, Recette::class);
     }
-
-    public function findNewestRecipes()
+    /**
+     * Trouve les recettes les plus récentes.
+     *
+     * @return Recette[] Les recettes les plus récentes
+     */
+    public function findNewestRecipes(): array
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->addOrderBy('r.dateCreated','DESC');
         $query = $queryBuilder->getQuery();
-
         $query->setMaxResults(4);
         return $query->getResult();
     }
-
-    public function findBestRecipes()
+    /**
+     * Trouve les recettes les mieux notées.
+     *
+     * @return Recette[] Les recettes les mieux notées
+     */
+    public function findBestRecipes(): array
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->addOrderBy('r.Note','DESC');
         $query = $queryBuilder->getQuery();
-
         $query->setMaxResults(4);
         return $query->getResult();
     }
-
-    public function findEntrees(int $page, int $limit): Paginator
+    /**
+     * Trouve les entrées avec pagination.
+     *
+     * @param int $page Le numéro de page
+     * @param int $limit Le nombre limite de résultats par page
+     * @return Paginator Les entrées avec pagination
+     */
+    public function findEntrees(int $page,
+                                int $limit): Paginator
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->andWhere('r.Categorie = 1');
         $queryBuilder->addOrderBy('r.Note', 'DESC');
         $query = $queryBuilder->getQuery();
-
         $query->setFirstResult(($page-1) * $limit);
         $query->setMaxResults($limit);
         return new Paginator($query, true);
     }
-
-    public function findPlats(int $page, int $limit)
+    /**
+     * Trouve les plats avec pagination.
+     *
+     * @param int $page Le numéro de page
+     * @param int $limit Le nombre limite de résultats par page
+     * @return Paginator Les entrées avec pagination
+     */
+    public function findPlats(int $page,
+                              int $limit): Paginator
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->andWhere('r.Categorie = 2');
         $queryBuilder->addOrderBy('r.Note', 'DESC');
         $query = $queryBuilder->getQuery();
-
         $query->setFirstResult(($page-1) * $limit);
         $query->setMaxResults($limit);
         return new Paginator($query, true);
     }
-
-    public function findDesserts(int $page, int $limit)
+    /**
+     * Trouve les desserts avec pagination.
+     *
+     * @param int $page Le numéro de page
+     * @param int $limit Le nombre limite de résultats par page
+     * @return Paginator Les entrées avec pagination
+     */
+    public function findDesserts(int $page,
+                                 int $limit): Paginator
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->andWhere('r.Categorie = 3');
         $queryBuilder->addOrderBy('r.Note', 'DESC');
         $query = $queryBuilder->getQuery();
-
         $query->setFirstResult(($page-1) * $limit);
         $query->setMaxResults($limit);
         return new Paginator($query, true);
     }
-
-    public function findRecipeById(int $id)
+    /**
+     * Trouve une recette par son ID.
+     *
+     * @param int $id L'ID de la recette
+     * @return mixed La recette
+     */
+    public function findRecipeById(int $id): mixed
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->select('r', 'ri', 'c', 'a', 'ing');
@@ -91,26 +120,29 @@ class RecetteRepository extends ServiceEntityRepository
         $queryBuilder->where('r.id = :id');
         $queryBuilder->setParameter('id',$id);
         $query = $queryBuilder->getQuery();
-
         return $query->getOneOrNullResult();
     }
-
-    public function findRecipesBySearch($searchTerm, $ingredientFilter)
+    /**
+     * Trouve les recettes par terme de recherche et/ou filtre d'ingrédient.
+     *
+     * @param string|null $searchTerm Le terme de recherche
+     * @param int|null $ingredientFilter L'ID du filtre d'ingrédient
+     * @return mixed Les recettes correspondant aux critères de recherche
+     */
+    public function findRecipesBySearch(?string $searchTerm,
+                                        ?int $ingredientFilter): mixed
     {
         $queryBuilder = $this->createQueryBuilder('r');
         $queryBuilder->leftJoin('r.ingredients', 'ri');
         $queryBuilder->leftJoin('ri.Ingredient', 'i');
-
         if ($searchTerm){
             $queryBuilder->andWhere('r.Nom LIKE :searchTerm');
             $queryBuilder->setParameter('searchTerm','%'.$searchTerm. '%');
         }
-
         if ($ingredientFilter){
             $queryBuilder->andWhere('i.id = :ingredientId');
             $queryBuilder->setParameter('ingredientId', $ingredientFilter);
         }
-
         return $queryBuilder->getQuery()->getResult();
     }
 }
